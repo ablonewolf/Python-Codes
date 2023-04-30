5f77a3df4d601db72aef7cbeed95f13a69651e19
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-04-30 21:50:37 +0600

    changed some of the variables.

--- For Loops/RightUpperTriangle.py
+++ For Loops/RightUpperTriangle.py
@@ -1,6 +1,6 @@
 def draw(level):
     for line in reversed(range(1, level + 1)):
-        for item in range(1, level - line + 1):
+        for space in range(1, level - line + 1):
             print("  ", end="")
         for item in reversed(range(1, line + 1)):
             if (item == 1):
