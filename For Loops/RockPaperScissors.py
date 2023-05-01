e5c0fb983b5136e64f4aaa447936d391d30cc1b8
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-05-01 12:41:54 +0600

    looped version of rock paper scissors game.

---
+++ For Loops/RockPaperScissors.py
@@ -0,0 +1,65 @@
+import random
+
+print('...rock...')
+print('...paper...')
+print('...scissors...')
+
+options = ['rock', 'paper', 'scissors']
+
+
+def takeInputs():
+    player_1_choice = input('Player 1 chooses: ')
+    if (player_1_choice not in options):
+        return None, None
+    else:
+        computer_choice = options[random.randint(0, 2)]
+        print(f'Computer chosses: {computer_choice}')
+        return player_1_choice, computer_choice
+
+
+def determineWinner(choice_1, choice_2):
+
+    if (choice_1.lower() == 'rock' and choice_2.lower() == 'paper'):
+        print('SHOOT!')
+        print('Computer wins this round.')
+        determineWinner.computer_score += 1
+        return True
+    elif (choice_1.lower() == 'scissors' and choice_2.lower() == 'rock'):
+        print('SHOOT!')
+        print('Computer wins this round.')
+        determineWinner.computer_score += 1
+        return True
+    elif (choice_1.lower() == 'paper' and choice_2.lower() == "scissors"):
+        print('SHOOT!')
+        print('Computer wins this round.')
+        determineWinner.computer_score += 1
+        return True
+    elif (choice_1.lower() == choice_2.lower()):
+        print('It\'s a tie!')
+        return False
+    else:
+        print('SHOOT!')
+        print('You win this round.')
+        determineWinner.player_score += 1
+        return True
+
+
+def game():
+    print("The game will be played in a round of 3. If it's a tie, it won't be considered a round.")
+    while (game.no_of_plays < 3):
+        choice_1, choice_2 = takeInputs()
+        if ((choice_1 is not None and choice_2 is not None) and (choice_1.lower() in options and choice_2.lower() in options)):
+            determineWinner.computer_score = 0
+            determineWinner.player_score = 0
+            if (determineWinner(choice_1, choice_2)):
+                game.no_of_plays += 1
+        else:
+            print('Invalid choice as inputs. Please try again')
+    if (determineWinner.computer_score > determineWinner.player_score):
+        print("Computer wins the game. Better luck next time :(")
+    else:
+        print("Congrats! You win the game.")
+
+
+game.no_of_plays = 0
+game()
