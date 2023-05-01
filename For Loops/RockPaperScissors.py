2442c35e1d3667503dd0031d18498393415f93f5
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-05-01 13:02:45 +0600

    modified the game logic.

--- For Loops/RockPaperScissors.py
+++ For Loops/RockPaperScissors.py
@@ -6,6 +6,8 @@ print('...scissors...')
 
 options = ['rock', 'paper', 'scissors']
 
+# method to take inputs
+
 
 def takeInputs():
     player_1_choice = input('Player 1 chooses: ')
@@ -15,6 +17,8 @@ def takeInputs():
         computer_choice = options[random.randint(0, 2)]
         print(f'Computer chosses: {computer_choice}')
         return player_1_choice, computer_choice
+
+# method to determine the winner of the round
 
 
 def determineWinner(choice_1, choice_2):
@@ -46,19 +50,20 @@ def determineWinner(choice_1, choice_2):
 
 def game():
     print("The game will be played in a round of 3. If it's a tie, it won't be considered a round.")
-    while (game.no_of_plays < 3):
+    determineWinner.computer_score = 0
+    determineWinner.player_score = 0
+    # check whether the number of games played is less than 3 and both of the scores are less than 2. if not, we found a winner
+    while (game.no_of_plays < 3 and (determineWinner.player_score < 2 and determineWinner.computer_score < 2)):
         choice_1, choice_2 = takeInputs()
         if ((choice_1 is not None and choice_2 is not None) and (choice_1.lower() in options and choice_2.lower() in options)):
-            determineWinner.computer_score = 0
-            determineWinner.player_score = 0
             if (determineWinner(choice_1, choice_2)):
                 game.no_of_plays += 1
         else:
             print('Invalid choice as inputs. Please try again')
     if (determineWinner.computer_score > determineWinner.player_score):
-        print("Computer wins the game. Better luck next time :(")
+        print("Computer wins the game. Better luck next time 😥.")
     else:
-        print("Congrats! You win the game.")
+        print("Congrats! You win the game😁😁😁")
 
 
 game.no_of_plays = 0
