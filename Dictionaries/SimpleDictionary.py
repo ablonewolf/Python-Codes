5b6ad66c852d9693182930a327210410b343bc5c
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-05-03 03:07:04 +0600

    demonstrated how to iterate over a dictionary.

---
+++ Dictionaries/SimpleDictionary.py
@@ -0,0 +1,11 @@
+Person = {
+    "name": "Arka Bhuiyan",
+    "age": 25,
+    "sex": "male",
+    "email": "arka.bhuiyan@gmail.com",
+    "profession": "Software Engineer",
+    "status": "single"
+}
+
+for key, value in Person.items():
+    print(f"{key.capitalize()}: {value}")
