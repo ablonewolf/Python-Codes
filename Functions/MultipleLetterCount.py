90f3f2ddc0c94ed4f030c69a711161ceb651496b
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-05-05 22:38:33 +0600

    code to find count of each letter in a string.

---
+++ Functions/MultipleLetterCount.py
@@ -0,0 +1,32 @@
+def check_character(char):
+    return (ord(char) >= 65 and ord(char) <= 90 or ord(char) >= 97 and ord(char) <= 122)
+
+
+def multiple_letter_count(string):
+    if len(string) == 0:
+        return None
+    return {letter.lower(): string.lower().count(letter.lower()) for letter in string if check_character(letter)}
+
+
+def take_input():
+    string = input("Enter the string: ")
+    if len(string) == 0:
+        print("Empty string not accepted. Please try again.")
+    else:
+        return string
+
+
+def print_letter_count(dictionary):
+    for (key, value) in dictionary.items():
+        print(f"Count for {key}: {value}")
+
+
+def main_menu():
+    string = take_input()
+    if (string == None):
+        main_menu()
+    letter_count = multiple_letter_count(string)
+    print_letter_count(letter_count)
+
+
+main_menu()
