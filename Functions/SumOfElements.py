127547a288d6deda21d2f8a0608fad25b09e03b7
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-05-05 23:32:27 +0600

    code for sum of variable number of integer values.

---
+++ Functions/SumOfElements.py
@@ -0,0 +1,25 @@
+def sum_of_elements(args):
+    print(args)
+    sum = 0
+    for num in args:
+        sum += num
+    return sum
+
+
+def take_input():
+    print("Enter numbers to calculate their sum or hit enter to exit providing input")
+    numbers = input("Enter numbers: ")
+
+    if len(numbers) == 0:
+        return None
+    else:
+        numbers = tuple(int(item) for item in numbers.split(' ') if item != '')
+        return numbers
+
+
+def main_menu():
+    numbers = take_input()
+    print(f"The sum of {numbers} is: {sum_of_elements(numbers)}")
+
+
+main_menu()
