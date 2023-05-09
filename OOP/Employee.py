375baee338c7fcbb878761e3c44045326bfe0fb7
Arka Bhuiyan <arkabhuiyancsedu@gmail.com>
2023-05-09 08:42:05 +0600

    Added a static method which is called class method in python.

--- OOP/Employee.py
+++ OOP/Employee.py
@@ -11,6 +11,18 @@ class Employee:
         print(f"Name of the employee: {self.name}, employee ID: {self.employee_id} and his preferred programming "
               f"language is {self.programming_language}")
 
+    @classmethod
+    def get_active_employees(cls):
+        return cls.active_employee
+
 
 Arka = Employee("Arka Bhuiyan", 99178, "Java")
+Farhan = Employee("Farhan Zaman", 11514, "JavaScript")
+print(f"Current number of total active employees is {Employee.get_active_employees()}")
+Nipa = Employee("Nipa Howladar", 11513, "Java")
+Akif = Employee("Akif Azwad", 11507, "Python")
+print(f"Current number of total active employees is {Employee.get_active_employees()}")
 Arka.print_info()
+Farhan.print_info()
+Nipa.print_info()
+Akif.print_info()
