f883d72156adc17abc9ee1d53e614aabcd2614d2
Arka Bhuiyan <arkabhuiyancsedu@gmail.com>
2023-05-09 08:53:32 +0600

    added another class method to create an employee with given parameters

--- OOP/Employee.py
+++ OOP/Employee.py
@@ -15,6 +15,11 @@ class Employee:
     def get_active_employees(cls):
         return cls.active_employee
 
+    @classmethod
+    def user_from_string(cls, data_string):
+        name, employee_id, programming_language = data_string.split(',')
+        return cls(name, employee_id, programming_language)
+
 
 Arka = Employee("Arka Bhuiyan", 99178, "Java")
 Farhan = Employee("Farhan Zaman", 11514, "JavaScript")
@@ -22,7 +27,11 @@ print(f"Current number of total active employees is {Employee.get_active_employe
 Nipa = Employee("Nipa Howladar", 11513, "Java")
 Akif = Employee("Akif Azwad", 11507, "Python")
 print(f"Current number of total active employees is {Employee.get_active_employees()}")
+
+Mosfik = Employee.user_from_string("Mosfikur Rahman,11502,Java")
+print(f"Current number of total active employees is {Employee.get_active_employees()}")
 Arka.print_info()
 Farhan.print_info()
 Nipa.print_info()
 Akif.print_info()
+Mosfik.print_info()
