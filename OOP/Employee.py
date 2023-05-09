6c2ec3888732a6b1d4c4ad87727879e96b12aa72
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-05-10 04:57:26 +0600

    Used property annotation to define a getter for age property. Used age.setter annotation to define a setter for age.

--- OOP/Employee.py
+++ OOP/Employee.py
@@ -1,19 +1,25 @@
 class Employee:
     active_employee = 0
 
-    def __init__(self, name, employee_id, programming_language):
+    def __init__(self, name, employee_id, age):
         self.name = name
         self.employee_id = employee_id
-        self.programming_language = programming_language
-        Employee.active_employee += 1
+        self._age = age
+        Employee.active_employee +=  1
 
     def __repr__(self):
-        return f"{self.name} is an employee with ID no: {self.employee_id}. This employee prefers " \
-               f"{self.programming_language} as his primary programming language."
+        return f"{self.name} is an employee with ID no: {self.employee_id}. This employee is {self.age} years old. "
 
-    def print_info(self):
-        print(f"Name of the employee: {self.name}, employee ID: {self.employee_id} and his preferred programming "
-              f"language is {self.programming_language}")
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if age >= 0:
+            self._age = age
+        else:
+            self._age = 0
 
     @classmethod
     def get_active_employees(cls):
@@ -21,26 +27,22 @@ class Employee:
 
     @classmethod
     def user_from_string(cls, data_string):
-        name, employee_id, programming_language = data_string.split(',')
-        return cls(name, employee_id, programming_language)
+        name, employee_id, age = data_string.split(',')
+        return cls(name, employee_id, age)
 
 
-Arka = Employee("Arka Bhuiyan", 99178, "Java")
-Farhan = Employee("Farhan Zaman", 11514, "JavaScript")
+Arka = Employee("Arka Bhuiyan", 99178, "25")
+Farhan = Employee("Farhan Zaman", 11514, "25")
 print(f"Current number of total active employees is {Employee.get_active_employees()}")
-Nipa = Employee("Nipa Howladar", 11513, "Java")
-Akif = Employee("Akif Azwad", 11507, "Python")
+Nipa = Employee("Nipa Howladar", 11513, "26")
+Akif = Employee("Akif Azwad", 11507, "26")
 print(f"Current number of total active employees is {Employee.get_active_employees()}")
 
-Mosfik = Employee.user_from_string("Mosfikur Rahman,11502,Java")
+Mosfik = Employee.user_from_string("Mosfikur Rahman,11502,26")
 print(f"Current number of total active employees is {Employee.get_active_employees()}")
 print(Arka)
-Arka.print_info()
 print(Farhan)
-Farhan.print_info()
 print(Nipa)
-Nipa.print_info()
 print(Akif)
-Akif.print_info()
 print(Mosfik)
-Mosfik.print_info()
+
