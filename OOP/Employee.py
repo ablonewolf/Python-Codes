6990b23fd4954c9ecf5f2d246ea752a6c4b4eec6
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-05-10 06:08:39 +0600

    Created a child class of Employee named Engineer. Used the super keyword and have overridden some methods.

--- OOP/Employee.py
+++ OOP/Employee.py
@@ -3,9 +3,9 @@ class Employee:
 
     def __init__(self, name, employee_id, age):
         self.name = name
-        self.employee_id = employee_id
-        self._age = age
-        Employee.active_employee +=  1
+        self.employee_id = int(employee_id)
+        self._age = int(age)
+        Employee.active_employee += 1
 
     def __repr__(self):
         return f"{self.name} is an employee with ID no: {self.employee_id}. This employee is {self.age} years old. "
@@ -31,18 +31,45 @@ class Employee:
         return cls(name, employee_id, age)
 
 
-Arka = Employee("Arka Bhuiyan", 99178, "25")
-Farhan = Employee("Farhan Zaman", 11514, "25")
-print(f"Current number of total active employees is {Employee.get_active_employees()}")
-Nipa = Employee("Nipa Howladar", 11513, "26")
-Akif = Employee("Akif Azwad", 11507, "26")
-print(f"Current number of total active employees is {Employee.get_active_employees()}")
+class Engineer(Employee):
+    def __init__(self, name, employee_id, age, programming_language):
+        super().__init__(name, employee_id, age)
+        self._programming_language = programming_language
 
-Mosfik = Employee.user_from_string("Mosfikur Rahman,11502,26")
+    def __int__(self, name, employee_id, age, programming_language):
+        super.__init__(name, employee_id, age)
+        self._programming_language = programming_language
+
+    def __repr__(self):
+        return f"{self.name} is an employee with ID no: {self.employee_id}. This employee is {self.age} years old " \
+               f"and has a preference on {self.programming_language} as a programming language."
+
+    @property
+    def programming_language(self):
+        return self._programming_language
+
+    @programming_language.setter
+    def programming_language(self, programming_language):
+        self._programming_language = programming_language
+
+    @classmethod
+    def user_from_string(cls, data_string):
+        name, employee_id, age, programming_language = data_string.split(', ')
+        return cls(name, employee_id, age, programming_language)
+
+
+Farhan = Employee("Farhan Zaman", 11514, 25)
+print(f"Current number of total active employees is {Employee.get_active_employees()}")
+Arka = Engineer("Arka Bhuiyan", 99178, 25, "Java")
+Nipa = Employee("Nipa Howladar", 11513, 25)
+Akif = Employee("Akif Azwad", 11507, 26)
+print(f"Current number of total active employees is {Employee.get_active_employees()}")
+Zareen = Employee.user_from_string("Zareen Zia, 11503, 23")
+Mosfik = Engineer.user_from_string("Mosfikur Rahman, 11502, 26, Python")
 print(f"Current number of total active employees is {Employee.get_active_employees()}")
 print(Arka)
 print(Farhan)
 print(Nipa)
 print(Akif)
 print(Mosfik)
-
+print(Zareen)
